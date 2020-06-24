8577a79f61dd93730ef1f04803384fba0f223711
takeke <takeke0404@yahoo.co.jp>
2020-06-24 23:00:27 +0900

    テスト用html更新

--- server/src/server.py
+++ server/src/server.py
@@ -1,9 +1,17 @@
 from websocket_server import WebsocketServer
 from datetime import datetime
 
+#コネクション接続時に呼ばれる関数
 def new_client(client, server):
+    #コネクションを確立している全体に送信
     server.send_message_to_all(datetime.now().isoformat() + ": new client joined!")
+
+#クライアント側からメッセージが飛んできたとき
+def message_recieve(client, server, message):
+    #メッセージを送ってきた全体に送信
+    server.send_message(client,message)
 
 server = WebsocketServer(7532, host="0.0.0.0")
 server.set_fn_new_client(new_client)
+server.set_fn_message_received(message_recieve)
 server.run_forever()
